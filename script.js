4b45ee5014919f671eeac5deed55de32c06da3d3
Kelven Aguiar <116621351+Kelven-aguiar@users.noreply.github.com>
2022-12-01 10:49:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// get filter element
 const filter = document.querySelector('#filter')
-// get cards elements
 const cards = document.querySelectorAll('.cards li')
-console.log(cards)
-// add input event for the filter element
 filter.addEventListener('input', inputFilter)
-// filter function
 function inputFilter(){
   if(filter != ""){cards.forEach(card => {
     let cardmin = card.querySelector('h2').textContent.toLowerCase()
@@ -17,22 +12,4 @@ function inputFilter(){
       card.removeAttribute('Style')
     }
   });
-}}
-
-  // if the filter is not empty
-
-    // for each card of cards
-
-      // get card heading (title)
-
-      // tranform to lower case
-
-      // transform filter text to lower case
-      
-      // if card title does not include the filter text
-        // hide the card element
-      // else
-        // unhide the card element
-  // else
-    // for each card of cards
-      // unhide the card element
+  }}
